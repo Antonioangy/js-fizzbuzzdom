82a48eedcd64ddf0e95b7e83b49cf97614719c6e
Antonioangy <dumitruangy26@gmail.com>
2021-10-25 16:29:25 +0200

    aggiunta quadrati

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Scrivi un programma che esegua un ciclo da 1 a 100
 // e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice
-let quadrati = document.getElementById("quadrato");
+let main = document.getElementById("main");
 // nomi quadrati
 let fizz = "fizz"
 let buzz = "buzz"
@@ -8,9 +8,12 @@ let fizzbuzz = "fizzbuzz"
 
 // per i multipli di 3,
 // per i multipli di 5
-for (let i = 3){
-    
+for (let i = 1; i<=100; i+=1){
+    main.innerHTML += "<div class='quadrato d-flex flex-wrap'></div>";
+                  
+
+
 }
 
-if (quadrati %)
+
 // e per i valori che sono sia multipli di 3 che di 5.
